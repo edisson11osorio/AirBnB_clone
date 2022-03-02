d263dac7be28c431dc958bd44aafd4dc8bb1014a
Migdalis <miglepage@gmail.com>
2022-03-01 22:51:43 -0500

    Change the documentation of the class and methods, add a line in __str__ and review the pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,32 @@
 #!/usr/bin/python3
-
+"""Module for the Base class"""
 from uuid import uuid4
 from datetime import datetime
 
-class BaseModel:
 
+class BaseModel:
+    """Class that defines all common attributes/methods for other classes"""
     def __init__(self):
-        """ init__ metho"""
+        """Constructor of the class"""
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-
     def __str__(self):
-        """__str__ method"""
-        return "[{}] ({}) {}"\
+        """Return the represents of the class objects as a string"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
-        """seve method"""
+        """Updates the public instance attribute updated_at with
+            the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """seve to_dict"""
+        """Returns a dictionary containing all keys/values of
+            __dict__ of the instance"""
         serialized = dict(self.__dict__)
         serialized["__class__"] = type(self).__name__
         serialized["created_at"] = serialized["created_at"].isoformat()
         serialized["updated_at"] = serialized["updated_at"].isoformat()
         return serialized
-        
