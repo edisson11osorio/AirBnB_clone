76811fcaa7fa708e97f8747861a97956edf57d53
edisson11osorio <osorioedisson08@gmail.com>
2022-03-06 12:03:50 -0500

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -7,26 +7,24 @@ from datetime import datetime
 import models
 
 
-class BaseModel: 
+class BaseModel:
     """Base Model"""
 
-    #def __init__(self, *args, **kwargs):
-       # """initialize the base model"""
-        #if kwargs:
-            
-
-
+    # def __init__(self, *args, **kwargs):
+    #     """initialize the base model"""
+    #     if kwargs:
     def __str__(self):
         """Return the represents of the class objects as a string"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                     self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id, self.__dict__)
 
     def save(self):
-        """Updates the public instance attribute updated_at with the current datetime"""
+        """Updates the public instance attribute
+         updated_at with the current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
-    
-        return 
+        """Returns a dictionary containing all keys/values of
+         __dict__ of the instance"""
+        return
